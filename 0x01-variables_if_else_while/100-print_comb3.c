3ff8ed72b304364b7142f74a5e2121a7dff5e647
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-14 08:18:28 -0700

    advanced tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0 ; n1 < 9 ; n1++)
+	{
+		for (n2 = 1 ; n2 < 10 ; n2++)
+		{
+			if (n2 > n1)
+			{
+				putchar(n1 + 48);
+				putchar(n2 + 48);
+
+				if (n1 != 8 || n2 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1, n2, n3;
+
+	for (n1 = 0 ; n1 < 8 ; n1++)
+	{
+		for (n2 = 1 ; n2 < 9 ; n2++)
+		{
+			for (n3 = 2 ; n3 < 10 ; n3++)
+			{
+				if (n2 > n1 && n3 > n2)
+				{
+					putchar(n1 + 48);
+					putchar(n2 + 48);
+					putchar(n3 + 48);
+
+					if (n1 != 7 || n2 != 8 || n3 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
