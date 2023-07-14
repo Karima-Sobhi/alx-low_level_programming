846b56d45efdf0bc3383550ed9c820fb7d1523df
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-14 06:16:51 -0700

    updates

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char ch = 'a' ;
+		int n = 26;
+        while (n--)
+        {
+			putchar(ch++);
+		}    
+		putchar('\n');
+        return (0);
+}
