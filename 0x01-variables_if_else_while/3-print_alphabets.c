61f26d93eaba3dbfa21639752448be1657d26bf3
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-14 06:39:20 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch1 = 'a' , ch2 = 'A';
+	int n = 26;
+
+	while (n--)
+	{
+		putchar(ch1++);
+	}
+	n = 26;
+	while (n--)
+	{
+		putchar(ch2++);
+	}
+	putchar('\n');
+	return (0);
+}
