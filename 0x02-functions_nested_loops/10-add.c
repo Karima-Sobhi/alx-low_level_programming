926da4bdbd30338c49477ccaa99987dc3116148f
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-18 13:18:28 -0700

    finL Mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result.
+ * @n1: first number.
+ * @n2: second number.
+ * Return: sum.
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all numbers from n to 98, followed by a new line.
+ * @n: a number .
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 98; i++)
+	{
+		if (i != 98)
+		{
+			if (i < 10)
+			{
+				_putchar(i + '0');
+			}
+			else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
