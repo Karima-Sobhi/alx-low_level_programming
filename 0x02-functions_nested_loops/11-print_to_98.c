792d6943c989982d5cd243dabb047e94e4c0766b
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-18 13:26:06 -0700

    final commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 98; i++)
+	for (i = n; i <= 98; i++)
 	{
 		if (i != 98)
 		{
@@ -25,6 +25,12 @@ void print_to_98(int n)
 
 			_putchar(',');
 			_putchar(' ');
+		}
+		else
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+
 		}
 	}
 }
