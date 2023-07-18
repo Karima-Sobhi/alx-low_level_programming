537da742fc7bfffa082096ceeea62627da24bb39
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-18 16:16:20 -0700

    advanced tasks

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,20 +8,20 @@
 
 int main(void)
 {
-	unsigned long fib[50];
+	unsigned long fib[52];
 	int i;
 
 	fib[0] = 0;
 	fib[1] = 1;
 
-	for (i = 2; i < 50; i++)
+	for (i = 2; i <= 51; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 	}
-	for (i = 0; i <= 50; i++)
+	for (i = 1; i <= 51; i++)
 	{
 		printf("%lu", fib[i]);
-		if (i == 49)
+		if (i == 51)
 			printf("\n");
 		else
 			printf(", ");
