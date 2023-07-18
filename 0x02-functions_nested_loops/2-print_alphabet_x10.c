b937258c98f28c532eb974ebc00d0bf37ec169b1
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-18 09:03:10 -0700

    mandatory tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,13 @@ void print_alphabet_x10(void)
 
 	while (n--)
 	{
-		print_alphabet();
-	}
+		char ch = 'a';
+		int n = 26;
 
+		while (n--)
+		{
+			_putchar(ch++);
+		}
+		_putchar('\n');
+	}
 }
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - a function that computes the absolute value of an integer.
+ * @c: number to be computed.
+ * Return: the absolute value of an integer.
+ */
+int _abs(int c)
+{
+	return (abs(c));
+}
