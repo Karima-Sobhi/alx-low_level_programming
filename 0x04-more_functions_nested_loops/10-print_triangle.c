0caebdef27d08da022b451db199b4415ba0b7823
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-20 11:29:30 -0700

    update last tasks

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,25 +1,29 @@
 #include "main.h"
-
 /**
- * print_triangle - prints a triangle, followed by a new line.
- * @size: the size of the triangle.
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
 void print_triangle(int size)
 {
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		int i, j;
 
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < (size - i); j++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (j = 0; j <= i; j++)
+
+			for (j = 1; j <= i; j++)
+			{
 				_putchar('#');
+			}
 
 			_putchar('\n');
 		}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,11 +1,15 @@
 #include "main.h"
+
 /**
- * print_square - prints a square, followed by a new line.
- * @size: the size of the square.
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
  */
 void print_square(int size)
 {
-	if (size > 0)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
 	{
 		int i, j;
 
@@ -18,5 +22,4 @@ void print_square(int size)
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
