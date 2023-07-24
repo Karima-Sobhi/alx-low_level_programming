2059f1f5d285ffe46a535150fc8e2e6c19e497ea
karima-sobhi <127060475+Karima-Sobhi@users.noreply.github.com>
2023-07-24 12:02:15 +0300

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to get it's length
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len+=1;
+		s++;
+	}
+	return (len);
+}
