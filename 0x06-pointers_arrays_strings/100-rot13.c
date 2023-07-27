a18fd50497f8cfda0c22a369fe100c8ea9e2cc2d
Karima Sobhi <karimasobhe010@gmail.com>
2023-07-27 06:58:55 -0700

    advanced task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: string to be encoded
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i = 0, j = 0;
+	char *s1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *s2 = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 2 * 26; j++)
+		{
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
