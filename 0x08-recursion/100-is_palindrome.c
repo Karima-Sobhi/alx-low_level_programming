a6a26bb9cf64a5f120ea09f059d5c55702b707e2
Karima Sobhi <karimasobhe010@gmail.com>
2023-08-02 09:50:44 -0700

    advanced task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+int is_palindrome_helper(char *s, int start, int end);
+
+/**
+ * is_palindrome_helper - compares the characters at the start and end
+ * @s: string to reserve
+ * @start: start of palindrome
+ * @end: end of palindrome
+ *
+ * Return: itself with start + 1 and end - 1
+ */
+
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ * Return: 1 true , 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int length = strlen(s);
+
+	return (is_palindrome_helper(s, 0, length - 1));
+}
