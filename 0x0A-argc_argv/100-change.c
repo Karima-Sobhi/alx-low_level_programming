95797ee565db327206f631b49ff86abd8cd4be72
Karima Sobhi <karimasobhe010@gmail.com>
2023-08-04 16:19:00 -0700

    update advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	if (argc == 2)
 	{
 		int i, leastcents = 0, money = atoi(argv[1]);
-		int cents[] = {25, 10, 5, 2, 1}
+		int cents[] = {25, 10, 5, 2, 1};
 
 		for (i = 0; i < 5; i++)
 		{
@@ -35,5 +35,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 
 }
