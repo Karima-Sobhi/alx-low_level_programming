38c8bd1287642ab354edded3b46b138a4effc659
Karima Sobhi <karimasobhe010@gmail.com>
2023-08-04 15:47:32 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point prints the function name
+ * @argc: number of argumnets
+ * @argv: values
+ * Return: Always 0 (Sucsees)
+ */
+
+void main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
