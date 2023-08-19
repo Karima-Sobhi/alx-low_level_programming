8de429104239221bda0f8d5797ce3e527378ec52
Karima Sobhi <karimasobhe010@gmail.com>
2023-08-19 05:49:16 -0700

    mandatory tasks

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @separator: tring to be printed between the strings
+ * @n: number of arguments
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	char *sep;
+	int i;
+
+	if (separator == NULL || *separator == 0)
+		sep = "";
+	else
+		sep = (char *) separator;
+
+
+	va_start(list, n);
+
+	if (n > 1)
+		printf("%s", va_arg(list, char *));
+	for (i = 1; i < n; i++)
+	{
+		printf("%s%s", sep, va_arg(list, char *));
+	}
+	va_end(list);
+	printf("\n");
+}
