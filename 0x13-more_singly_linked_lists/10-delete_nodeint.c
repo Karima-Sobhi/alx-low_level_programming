680077b46ddf8fe6736041d83072c9a048808895
Karima Sobhi <karimasobhe010@gmail.com>
2023-08-28 17:34:05 -0700

    ya rab

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,10 +9,11 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head, *current;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (!*head)
 		return (-1);
 
 	if (index == 0)
