302d1a7b4f10e06f7ddcdb9dfbd386f67968cf27
Karima Sobhi <karimasobhe010@gmail.com>
2023-08-28 17:06:32 -0700

    fix some errors

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,17 +6,18 @@
  * @head: list's head
  * Return: the head node’s data (n).
  */
-
 int pop_listint(listint_t **head)
 {
-	listint_t *temp = (*head)->next;
-	int n;
+	listint_t *temp;
+	int num;
 
 	if (!head || !*head)
 		return (0);
 
-	n = (*head)->n;
+	num = (*head)->n;
+	temp = (*head)->next;
 	free(*head);
 	*head = temp;
-	return (n);
+
+	return (num);
 }
